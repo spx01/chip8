5e4b8edfc356f0f4bbaf243a4d1c3c06897bcf91
spx01 <42381535+spx01@users.noreply.github.com>
2020-07-14 14:06:58 +0300

    invert colors instead of drawing a different character

--- inc/window.h
+++ inc/window.h
@@ -1,8 +1,8 @@
 #ifndef CHIP8_DISPLAY_H
 #define CHIP8_DISPLAY_H
 
-#define PRINT_FILL {waddch(c8Win, ACS_BLOCK); waddch(c8Win, ACS_BLOCK);}
-#define PRINT_EMPTY {waddstr(c8Win, "  ");}
+#define PRINT_FILL {wattron(c8Win, A_REVERSE); waddstr(c8Win, "  ");}
+#define PRINT_EMPTY {wattroff(c8Win, A_REVERSE); waddstr(c8Win, "  ");}
 #define PRINT_PIXEL_WIDTH 2
 
 void initWindow();
